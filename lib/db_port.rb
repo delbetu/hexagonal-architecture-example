12f6762977563e4d3f4d523fc37cea3be9c729c0
Marcos Bellucci <mbellucci@salesforce.com>
2017-08-30 09:28:06 -0300

    Allow db port to choose several adapters

--- lib/db_port.rb
+++ lib/db_port.rb
@@ -2,7 +2,15 @@ require_relative 'db_port/dm_adapter'
 
 # provides different kinds of adapters
 module DB
-  def self.build
-    DMAdapter.instance
+  DATAMAPPER_PG = 1
+  class NoAdapterFound < StandardError; end
+
+  def self.build(adapter_key)
+    case adapter_key
+    when DATAMAPPER_PG
+      DMAdapter.instance
+    else
+      raise NoAdapterFound, adapter_key
+    end
   end
 end
--- spec/lib/db_port_spec.rb
+++ spec/lib/db_port_spec.rb
@@ -2,9 +2,15 @@ require_relative '../../lib/db_port'
 
 describe DB do
   describe '.build' do
-    it 'returns a datamapper adapter' do
-      response = DB.build
+    it 'pick up the right adapter' do
+      response = DB.build(DB::DATAMAPPER_PG)
       expect(response).to be_a(DB::DMAdapter)
+    end
+
+    context 'when adapter does not exist' do
+      it 'raises an error' do
+        expect { DB.build('no exists') }.to raise_error(DB::NoAdapterFound)
+      end
     end
   end
 end
